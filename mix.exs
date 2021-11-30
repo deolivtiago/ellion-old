f8b239a01c4ee4ae3e7c6557075f712e2de0dde8
Tiago Oliveira <olvrti@gmail.com>
2021-11-30 11:48:16 -0300

    chore: include credo 1.6

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Ellion.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.6.2"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
