f9cc93d61a5369bdbc9a51aaa46694332647de7e
Tiago de Oliveira <deoliv.tiago@proton.me>
2022-06-01 22:11:44 -0300

    feat: add country view

---
+++ lib/ellion_web/views/country_view.ex
@@ -0,0 +1,30 @@
+defmodule EllionWeb.CountryView do
+  use EllionWeb, :view
+
+  alias EllionWeb.CountryView
+
+  @doc """
+  Renders a list of countries.
+  """
+  def render("index.json", %{countries: countries}) do
+    %{data: render_many(countries, CountryView, "country.json")}
+  end
+
+  @doc """
+  Renders a single country.
+  """
+  def render("show.json", %{country: country}) do
+    %{data: render_one(country, CountryView, "country.json")}
+  end
+
+  @doc """
+  Renders country data.
+  """
+  def render("country.json", %{country: country}) do
+    %{
+      id: country.id,
+      code: country.code,
+      name: country.name
+    }
+  end
+end
